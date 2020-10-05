60c26c600a91a4aa69e86279007e9994117e449b
Christopher Lee <RC0043703@student.riversidecollege.ac.uk>
2020-10-05 15:06:48 +0100

    added try catch

--- numbers/Form1.cs
+++ numbers/Form1.cs
@@ -25,7 +25,7 @@ namespace numbers
             richTextBox1.AppendText(d1.ToString("0.0E0") + "\n");
             richTextBox1.AppendText(d1.ToString("0.00E00") + "\n");
             double pi = 3.141592653589793;
-             richTextBox1.AppendText(pi.ToString() + "\n");
+            richTextBox1.AppendText(pi.ToString() + "\n");
             richTextBox1.AppendText(pi.ToString("N") + "\n");
             richTextBox1.AppendText(pi.ToString("N3") + "\n");
             richTextBox1.AppendText(pi.ToString("N0") + "\n");
@@ -44,8 +44,35 @@ namespace numbers
             richTextBox1.AppendText(c.ToString("0.0") + "\n");
             richTextBox1.AppendText(c.ToString("0.0E0") + "\n");
             richTextBox1.AppendText(c.ToString("0.00E00") + "\n");
-                    }
-       
+            string st = "3";
+            try
+            {
+                int it = int.Parse(st);
+            }
+            catch
+            {
+                MessageBox.Show(st + " is not an integer");
+            }
+            string s2 = "3.2";
+            try
+            {
+                int i2 = int.Parse(s2);
+            }
+            catch
+            {
+                MessageBox.Show(s2 + " is not an interger");
+                int i2 = 0;
+            }
+            string s3 = "pi";
+            try
+            {
+                double d2 = double.Parse(s3);
+            }
+            catch
+            {
+                double d2 = 0;
+            }
+        }
 
         private void RichTextBox1_TextChanged(object sender, EventArgs e)
         {
